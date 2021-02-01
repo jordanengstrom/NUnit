b0fafd7e6424d848ba58424cd39d9d5b22e7565a
jordanengstrom <jordan.engstrom@gmail.com>
2021-01-31 17:47:27 -0700

    learned about testing reference equalities

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -1,5 +1,7 @@
 ﻿using Loans.Domain.Applications;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Loans.Tests
 {
@@ -11,7 +13,7 @@ namespace Loans.Tests
         {
             var sut = new LoanTerm(1);
 
-            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+            Assert.That(sut.ToMonths(), Is.EqualTo(12), "Months should be 12 * number of years.");
         }
 
         [Test]
@@ -19,6 +21,32 @@ namespace Loans.Tests
         {
             var sut = new LoanTerm(1);
             Assert.That(sut.Years, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
+
+        [Test]
+        public void ReferenceEqualityExample()
+        {
+            var a = new LoanTerm(1);
+            var b = a;
+            var c = new LoanTerm(1);
+
+            Assert.That(a, Is.SameAs(b));
+            Assert.That(a, Is.Not.SameAs(c));
+
+            var x = new List<String> { "a", "b" };
+            var y = x;
+            var z = new List<String> { "a", "b" };
+
+            Assert.That(y, Is.SameAs(x));
+            Assert.That(z, Is.Not.SameAs(x));
         }
     }
 }
